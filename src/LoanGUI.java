466f736704db305ac27e6df76cc4a99062cde4f2
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2018-12-05 10:26:56 -0500

    GUI complete, need to refactor out extra code

--- src/LoanGUI.java
+++ src/LoanGUI.java
@@ -24,6 +24,8 @@ public class LoanGUI extends JFrame implements ActionListener {
 	
 	ButtonGroup buttGroup = null;
 	
+	ArrayList<JLabel> labels = null;
+	
 	JTextField name = null;
 	
 	JTextField principle = null;
@@ -58,6 +60,9 @@ public class LoanGUI extends JFrame implements ActionListener {
         
         manager = new LoanManager();
         
+        setLayout(new GridBagLayout());
+        GridBagConstraints loc = null;
+        
         file = new JMenu("File");
         save = new JMenuItem("Save");
         save.addActionListener(this);
@@ -74,51 +79,69 @@ public class LoanGUI extends JFrame implements ActionListener {
         
         simpleInterest = new JRadioButton("Simple Interest");
         loc = new GridBagConstraints();
-        loc.gridx = column;
+        loc.gridx = column + 1;
         loc.gridy = row;  
         loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
-        loc.gridwidth = 2;
         add(simpleInterest, loc);
         
         amortized = new JRadioButton("Amortized");
         loc = new GridBagConstraints();
-        loc.gridx = column + 1;
+        loc.gridx = column + 2;
         loc.gridy = row;  
         loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
-        loc.gridwidth = 2;
         add(amortized, loc);
         
         buttGroup = new ButtonGroup();
         buttGroup.add(simpleInterest);
         buttGroup.add(amortized);
         
-        name = new JTextField("Name");
+        labels = new ArrayList<JLabel>();
+        labels.add(new JLabel("Loan Type"));
+        labels.add(new JLabel("Name"));
+        labels.add(new JLabel("Principle"));
+        labels.add(new JLabel("Length"));
+        labels.add(new JLabel("Rate"));
+        labels.add(new JLabel("Payment"));
+        
+        
+        for (JLabel l: labels) {
+        	loc = new GridBagConstraints();
+            loc.gridx = column;
+            loc.gridy = row;  
+            loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+            add(l, loc);
+            row++;
+        }
+        
+        row = 0;
+        
+        name = new JTextField(15);
         loc = new GridBagConstraints();
-        loc.gridx = column;
+        loc.gridx = column + 1;
         loc.gridy = row + 1;  
         loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
         loc.gridwidth = 2;
         add(name, loc);
         
-        principle = new JTextField("Principle");
+        principle = new JTextField(15);
         loc = new GridBagConstraints();
-        loc.gridx = column;
+        loc.gridx = column + 1;
         loc.gridy = row + 2;  
         loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
         loc.gridwidth = 2;
         add(principle, loc);
         
-        length = new JTextField("Length");
+        length = new JTextField(15);
         loc = new GridBagConstraints();
-        loc.gridx = column;
+        loc.gridx = column + 1;
         loc.gridy = row + 3;  
         loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
         loc.gridwidth = 2;
         add(length, loc);
         
-        payment = new JTextField("Payment");
+        payment = new JTextField(15);
         loc = new GridBagConstraints();
-        loc.gridx = column;
+        loc.gridx = column + 1;
         loc.gridy = row + 5;  
         loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
         loc.gridwidth = 2;
@@ -127,18 +150,65 @@ public class LoanGUI extends JFrame implements ActionListener {
         rate = new JComboBox<Integer>(rates);
         rate.setSelectedIndex(0);
         loc = new GridBagConstraints();
-        loc.gridx = column;
+        loc.gridx = column + 1;
         loc.gridy = row + 4;  
         loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
         loc.gridwidth = 2;
         add(rate, loc);
         
+        search = new JButton("Search");
+        loc = new GridBagConstraints();
+        loc.gridx = column + 3;
+        loc.gridy = row + 1;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        search.addActionListener(this);
+        add(search, loc);
         
+        calculate = new JButton("Calculate");
+        loc = new GridBagConstraints();
+        loc.gridx = column + 3;
+        loc.gridy = row + 2;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        calculate.addActionListener(this);
+        add(calculate, loc);
         
+        add = new JButton("Add Loan");
+        loc = new GridBagConstraints();
+        loc.gridx = column + 3;
+        loc.gridy = row + 3;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        add.addActionListener(this);
+        add(add, loc);
+        
+        delete = new JButton("Delete Loan");
+        loc = new GridBagConstraints();
+        loc.gridx = column + 3;
+        loc.gridy = row + 4;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        delete.addActionListener(this);
+        add(delete, loc);
+        
+        clear = new JButton("Clear");
+        loc = new GridBagConstraints();
+        loc.gridx = column + 3;
+        loc.gridy = row + 5;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        clear.addActionListener(this);
+        add(clear, loc);
 
 	}
 	
 	public void actionPerformed(ActionEvent e) {
 		
+	}
+	
+	public static void main (String Args[]) {
+		LoanGUI gui = new LoanGUI();
+        gui.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        gui.setLocation(300, 300);
+        gui.setTitle("Loan Manager");
+        gui.setVisible(true);
+        gui.pack();
+
 	}
 }
