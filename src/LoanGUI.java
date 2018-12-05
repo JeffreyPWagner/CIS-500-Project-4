5492bce3d078afc08a854d08932d4920d3e11403
JeffreyPWagner <41526084+JeffreyPWagner@users.noreply.github.com>
2018-12-05 09:45:08 -0500

    Added half of GUI components

--- src/LoanGUI.java
+++ src/LoanGUI.java
@@ -22,11 +22,31 @@ public class LoanGUI extends JFrame implements ActionListener {
 	
 	JRadioButton amortized = null;
 	
-	ArrayList<JTextField> textFields = null;
+	ButtonGroup buttGroup = null;
 	
-	ArrayList<JButton> buttons = null;
+	JTextField name = null;
+	
+	JTextField principle = null;
+	
+	JTextField length = null;
+	
+	JTextField payment = null;
+	
+	JButton search = null;
+	
+	JButton calculate = null;
+	
+	JButton add = null;
+	
+	JButton delete = null;
+	
+	JButton clear = null;
+	
+	JComboBox<Integer> rate = null;
 	
 	GridBagConstraints loc = null;
+	
+	Integer[] rates = {3, 4, 5, 6, 7};
 	
 	public LoanGUI() {
         int column = 0;
@@ -51,6 +71,67 @@ public class LoanGUI extends JFrame implements ActionListener {
         menu = new JMenuBar();
         setJMenuBar(menu);
         menu.add(file);
+        
+        simpleInterest = new JRadioButton("Simple Interest");
+        loc = new GridBagConstraints();
+        loc.gridx = column;
+        loc.gridy = row;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        loc.gridwidth = 2;
+        add(simpleInterest, loc);
+        
+        amortized = new JRadioButton("Amortized");
+        loc = new GridBagConstraints();
+        loc.gridx = column + 1;
+        loc.gridy = row;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        loc.gridwidth = 2;
+        add(amortized, loc);
+        
+        buttGroup = new ButtonGroup();
+        buttGroup.add(simpleInterest);
+        buttGroup.add(amortized);
+        
+        name = new JTextField("Name");
+        loc = new GridBagConstraints();
+        loc.gridx = column;
+        loc.gridy = row + 1;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        loc.gridwidth = 2;
+        add(name, loc);
+        
+        principle = new JTextField("Principle");
+        loc = new GridBagConstraints();
+        loc.gridx = column;
+        loc.gridy = row + 2;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        loc.gridwidth = 2;
+        add(principle, loc);
+        
+        length = new JTextField("Length");
+        loc = new GridBagConstraints();
+        loc.gridx = column;
+        loc.gridy = row + 3;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        loc.gridwidth = 2;
+        add(length, loc);
+        
+        payment = new JTextField("Payment");
+        loc = new GridBagConstraints();
+        loc.gridx = column;
+        loc.gridy = row + 5;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        loc.gridwidth = 2;
+        add(payment, loc);
+
+        rate = new JComboBox<Integer>(rates);
+        rate.setSelectedIndex(0);
+        loc = new GridBagConstraints();
+        loc.gridx = column;
+        loc.gridy = row + 4;  
+        loc.insets = new Insets(topIns, leftIns, botIns, rightIns);
+        loc.gridwidth = 2;
+        add(rate, loc);
         
         
         
